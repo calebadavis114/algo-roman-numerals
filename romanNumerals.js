aba5a8e5c87aa3d5c0e63d10fc4c275af5917267
calebadavis114 <caleb.a.davis114@gmail.com>
2023-10-04 16:29:12 -0700

    finnished

--- romanNumerals.js
+++ romanNumerals.js
@@ -1,9 +1,52 @@
+const { objectExpression } = require("@babel/types");
+console.log(toRomanLazy(944));
 function toRomanLazy(num) {
-  return "";
+ const numerals = {
+    "M":1000,
+    "D":500,
+    "C":100,
+     "L":50,
+    "X":10,
+   "V":5,
+    "I":1
+ }
+let roman = "";
+for(key in numerals){
+  while(num >= numerals[key]){
+  if(num >= numerals[key]){
+   roman += key;
+    num = num - numerals[key];
+  }
+}
+}
+return roman;
 }
 
 function toRoman(num) {
-  return "";
+  const numerals = {
+    "M":1000,
+    "CM":900,
+    "D":500,
+    "CD":400,
+    "C":100,
+     "L":50,
+     "XL":40,
+    "X":10,
+    "IX":9,
+   "V":5,
+   "IV":4,
+    "I":1
+ }
+ let roman = "";
+for(key in numerals){
+  while(num >= numerals[key]){
+  if(num >= numerals[key]){
+   roman += key;
+    num = num - numerals[key];
+  }
+}
+}
+  return roman;
 }
 
 module.exports = { toRoman, toRomanLazy };
